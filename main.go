21c7994f5c03ca8ad89d9107b5f75592924d0da9
xavierfoguet <xavier.foguet@elindependiente.com>
2017-07-11 10:33:27 +0200

    wercker build time!

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func CityHandler(res http.ResponseWriter, req *http.Request) {
-	data, _ := json.Marshal("{'cities':'San Francisco, Amsterdam, Berlin, New York, Tokyo,Barcelona'}")
+	data, _ := json.Marshal("{'cities':'San Francisco, Amsterdam, Berlin, New York, Tokyo,Barcelona, Toledo'}")
 	res.Header().Set("Content-Type", "application/json; charset=utf-8")
 	res.Write(data)
 }
